576d5505f21fc0dcaf3832c5296e4a114d161f96
Devprashant05 <prashantsehgal03@gmail.com>
2025-09-18 13:39:33 +0530

    Connect db with auth and made sidebar card section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
     return (
         <div className="m-4">
             <h1>Home page content</h1>
